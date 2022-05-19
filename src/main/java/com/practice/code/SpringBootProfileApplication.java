8115521bd4efd48fb20991119eedeaae311cae09
@kuldeep_fc <kuldeep.argal@fewerclicks.in>
2022-05-19 11:49:54 +0530

    update code

---
+++ src/main/java/com/practice/code/SpringBootProfileApplication.java
@@ -0,0 +1,37 @@
+package com.practice.code;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+//@Profile("database")
+public class SpringBootProfileApplication {
+
+	@Value("${message}")
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfileApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String message() {
+		return "welcome to " + getMessage();
+	}
+
+}
